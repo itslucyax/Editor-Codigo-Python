d506fe90229974a824fc2244404cd8c1de90bbf0
itslucyax <luciameslava@gmail.com>
2026-02-27 13:37:07 +0100

    moved

--- test_bd.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Script para probar el editor con BD
-REM ANTES DE EJECUTAR: Edita test_config.json con tus datos reales
-echo Iniciando editor con conexion a BD...
-py -3 main.py --config-file test_config.json
-pause
--- test_local.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Script para probar el editor en modo local (sin BD)
-echo Iniciando editor en modo local...
-py -3 main.py --local
-pause
